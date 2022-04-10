76d322e6689c5f65ca3ffedda829be4676853d3d
tonyvu2508 <tonyvu2508@gmail.com>
2022-04-10 17:03:05 +0700

    netlify-config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 require('dotenv').config()
 const express = require('express')
+const serverless = require("serverless-http");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+// const app = express()
+// const PORT = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-app.listen(PORT, () => { console.log(`App is running on PORT: ${PORT}`) })
+// app.listen(PORT, () => { console.log(`App is running on PORT: ${PORT}`) })
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
